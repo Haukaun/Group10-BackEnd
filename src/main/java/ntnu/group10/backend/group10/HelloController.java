e3020b8279af06417e3d1e46e234748cdca433a9
PMolnes <pettermolnes@icloud.com>
2022-03-03 15:05:14 +0100

    Initialized the connection to the database, and setup a /hello endpoint for testing.

---
+++ src/main/java/ntnu/group10/backend/group10/HelloController.java
@@ -0,0 +1,15 @@
+package ntnu.group10.backend.group10;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Version 1.0";
+    }
+
+
+}
